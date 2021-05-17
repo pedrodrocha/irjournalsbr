26b46667bfb693d37fe4f5261a8b6c2f860692ca
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2021-05-17 19:44:55 -0300

    bjir

--- R/bjir.R
+++ R/bjir.R
@@ -216,7 +216,7 @@ bjir <- function(
     url_lido %>%
       rvest::html_nodes('meta[name="citation_pdf_url"]') %>%
       rvest::html_attr('content') -> pdf_url
-
+    if(length(pdf_url) == 0){pdf_url <- NA }
 
 
     build_data(
