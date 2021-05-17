73817319966bdcd1275fe1b9096b3305e48a14e0
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2021-05-17 19:34:45 -0300

    conjunturainternacional

--- R/bjir.R
+++ R/bjir.R
@@ -241,6 +241,17 @@ bjir <- function(
 
   })
 
-  bjir
+  bjir %>%
+    dplyr::group_by(TI) %>%
+    dplyr::mutate(
+      AU = toString(AU),
+      OG = toString(OG)
+    ) %>%
+    dplyr::distinct() %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      AB = stringr::str_squish(AB)
+    )
+
 
 }
