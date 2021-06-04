cb6268be5a0f511cdce45b1b40973a5f46206159
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2021-06-04 16:39:16 -0300

    mural

--- R/muralinternacional.R
+++ R/muralinternacional.R
@@ -204,6 +204,7 @@ muralinternacional <- function(
       rvest::html_nodes('meta[name="citation_pdf_url"]') %>%
       rvest::html_attr('content') -> pdf_url
 
+    if(length(pdf_url) == 0){pdf_url <- "NA" }
 
     build_data(
       authors = authors,
