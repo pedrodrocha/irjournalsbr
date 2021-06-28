720172d3c1929d7f7e0d5cef87ac023b5065f8e0
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2021-06-28 17:58:18 -0300

    arrumando bjlas

--- R/bjlas.R
+++ R/bjlas.R
@@ -243,6 +243,8 @@ bjlas <- function(
       rvest::html_nodes('meta[name="citation_pdf_url"]') %>%
       rvest::html_attr('content') -> pdf_url
 
+    if(length(pdf_url) == 0){pdf_url <- "NA" }
+
 
     build_data(
       authors = authors,
