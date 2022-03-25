17c77f34f944fdf7fd5095685b2f721a2efae5c3
Pedro D. Rocha <pedro.diniz.rocha@gmail.com>
2022-03-25 16:21:19 -0300

    Austral atualizada para OJS 3

--- R/austral.R
+++ R/austral.R
@@ -25,28 +25,28 @@ austral <- function(
 
   # PART 1: EDITIONS LINKS
 
-  url_archive <- "https://seer.ufrgs.br/austral/issue/archive"
+  url_archive <- "https://seer.ufrgs.br/index.php/austral/issue/archive"
 
   url_archive_lido <- xml2::read_html(url_archive)
 
   url_archive_lido %>%
-    rvest::html_nodes("h4 a") %>%
+    rvest::html_nodes(".obj_issue_summary .title") %>%
     rvest::html_attr("href") -> primary_url
 
 
   url_archive_lido %>%
-    rvest::html_nodes("h4 a") %>%
+    rvest::html_nodes(".obj_issue_summary .title") %>%
     rvest::html_text() %>%
-    stringr::str_remove(":(.*)") -> eds
+    stringr::str_trim() -> eds
 
   tibble::tibble(url = primary_url,
                  editions = eds) %>%
     dplyr::mutate(
-      vol = stringr::str_extract(editions, "(Vol [0-9]{2})|(Vol [0-9]{1})") %>%
-        stringr::str_replace_all(.,'Vol ','') %>%
+      vol = stringr::str_extract(editions, "(Vol. [0-9]{2})|(Vol. [0-9]{1})") %>%
+        stringr::str_replace_all(.,'Vol. ','') %>%
         as.integer(.),
-      n = stringr::str_extract(editions,'(No [0-9]{2})|(No [0-9]{1})') %>%
-        stringr::str_replace_all(.,'No ','') %>%
+      n = stringr::str_extract(editions,'(No. [0-9]{2})|(No. [0-9]{1})') %>%
+        stringr::str_replace_all(.,'No. ','') %>%
         as.integer(.),
       ano = stringr::str_extract(editions,"[0-9]{4}") %>%
         as.double(.),
@@ -57,6 +57,8 @@ austral <- function(
                     vol %in% volume) %>%
     dplyr::pull(url) -> primary_url
 
+
+
   # PART II: ARTICLES LINKS
 
   articles_url <- purrr::map(primary_url, function(x) {
@@ -64,26 +66,25 @@ austral <- function(
     url_lido <- xml2::read_html(x)
 
     url_lido %>%
-      rvest::html_nodes('.tocTitle a') %>%
-      rvest::html_attr('href')  -> links
+      rvest::html_nodes(".title a") %>%
+      rvest::html_attr("href") -> links
 
     url_lido %>%
-      rvest::html_nodes('.tocTitle a') %>%
-      rvest::html_text()  %>%
-      stringr::str_remove_all("\\n|\\t")-> names
+      rvest::html_nodes(".title a") %>%
+      rvest::html_text() %>%
+      stringr::str_remove_all("\\n|\\t") -> names
 
 
     tibble::tibble(
       links = links,
       names = names
     ) %>%
-      dplyr::filter(
-        !stringr::str_detect(names, '(Complete Edition)|(Complete edition)')
-      ) %>%
       dplyr::pull(links)
 
   }) %>%
     purrr::flatten_chr()
+
+
 
   # PART III: SCRAPPING METADATA
 
@@ -99,6 +100,14 @@ austral <- function(
     url_lido %>%
       rvest::html_nodes('meta[name="citation_author_institution"]') %>%
       rvest::html_attr('content') -> filiation
+
+    if (length(filiation) == 0) {
+      url_lido %>%
+        rvest::html_nodes('.affiliation') %>%
+        rvest::html_text() %>%
+        stringr::str_remove_all("\\n|\\t") -> filiation
+    }
+
 
     ## B) Authors
 
@@ -117,7 +126,8 @@ austral <- function(
 
     url_lido %>%
       rvest::html_node('meta[name="DC.Title"]') %>%
-      rvest::html_attr('content') -> title
+      rvest::html_attr('content') %>%
+      stringr::str_to_title() -> title
 
     ## D) Abstract
 
@@ -138,16 +148,16 @@ austral <- function(
 
     ## F) Pages
 
-    url_lido %>%
-      rvest::html_nodes('meta[name="DC.Identifier.pageNumber"]') %>%
-      rvest::html_attr('content') -> pages
 
-    if(length(pages) == 0){ pages <- NA }
+    pages <- NA
 
     ## G) Language
 
-    language <- "en"
+    url_lido %>%
+      rvest::html_nodes('meta[name="citation_language"]') %>%
+      rvest::html_attr('content') -> language
 
+    if(length(language) == 0){ language <- NA }
     ## H) Volume
 
     url_lido %>%
@@ -169,11 +179,12 @@ austral <- function(
       stringr::str_extract(.,'[0-9]{4}')-> year
 
     ## K) Keywords
-
+    query <- paste0("meta[name=citation_keywords][xml\\:lang='",language,"']" )
+    query
     url_lido %>%
-      rvest::html_nodes('meta[name="DC.Subject"][xml\\:lang="en"]') %>%
+      rvest::html_nodes(query) %>%
       rvest::html_attr('content') %>%
-      paste0(., collapse = ', ') -> keywords
+      paste0(., collapse = '; ') -> keywords
 
     if(keywords == ""){keywords <- NA}
 
@@ -187,11 +198,12 @@ austral <- function(
       rvest::html_nodes('meta[name="citation_pdf_url"]') %>%
       rvest::html_attr('content') -> pdf_url
 
+    if(length(pdf_url) == 0){pdf_url <- "NA"}
     if(language == 'pt') { pdf_url <- pdf_url[1]} # Portuguese pdf
-    else if(language == 'en') { pdf_url <- pdf_url[2]} # English pdf
+    if(language == 'en') { pdf_url <- pdf_url[2]} # English pdf
 
-    if(length(pdf_url == 0)){pdf_url <- "NA"}
-    if(pdf_url == ""){pdf_url <- "NA"}
+
+
 
 
     build_data(
